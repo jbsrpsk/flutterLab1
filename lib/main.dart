963b1b1a755a6003815c0556a5a1cd6e00aa1f1f
Jangbu Sherpa <sherpa.jangbu7sk@gmail.com>
2024-04-05 10:49:42 -0400

    Removed banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class SnakeGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Color(0xFF101010),
         textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
